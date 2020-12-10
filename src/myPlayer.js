eb7fc6757211c8b8bf1de80561c4d535e3e2daab
CocoH11 <55780171+CocoH11@users.noreply.github.com>
2020-12-10 15:19:58 +0100

    slot table added to player component

--- src/myPlayer.js
+++ src/myPlayer.js
@@ -16,6 +16,13 @@ Vue.component("myPlayer", {
     <label for="idPlayer">#player:</label><input id="idPlayer" v-model="idPlayer"><br> \
     <div v-if="player !== undefined"> \
       <h1>{{player.name}}, level {{player.level}}, life = {{player.life}}, strength =  {{player.strength}}</h1> \
+      <table border="1">
+        <tr v-for="sl in player.slots">
+          <td @dragover.prevent @drop="drop(sl.name)">{{sl.name}}</td>
+          <td v-for="it in sl.items">{{it.name}}</td>
+        </tr>
+      </table>
+      <br>
     </div> \
   </div>
   `
