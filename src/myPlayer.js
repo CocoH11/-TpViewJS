620d5242dff2d82e1e19f52f3a8072b54b1dff88
CocoH11 <55780171+CocoH11@users.noreply.github.com>
2020-12-11 14:50:25 +0100

    slot are updated on the server

--- src/myPlayer.js
+++ src/myPlayer.js
@@ -6,7 +6,7 @@ Vue.component("myPlayer", {
     <h1>{{player.name}}, level {{player.level}}, life = {{player.life}}, strength =  {{player.strength}}</h1> \
     <table border="1">
       <tr v-for="sl in player.slots">
-        <td @dragover.prevent @drop="drop(sl.name)">{{sl.name}}</td>
+        <td @dragover.prevent @drop="drop(sl)">{{sl.name}}</td>
         <td v-for="it in sl.items">{{it.name}}</td>
       </tr>
     </table>
@@ -27,8 +27,26 @@ Vue.component("myPlayer", {
       console.log("stop dragging");
     },
     drop(slot) {
-      console.log("item dropped on "+slot);
-      this.player.assign(this.idDrag,slot);
+      console.log("item dropped on "+slot.name);
+      if (this.player.assign(this.idDrag,slot.name)) {
+        this.updateSlot(slot);
+      }
+    },
+    updateSlot(slot){
+      data={
+        id:this.player._id,
+        slotName: slot.name,
+        items: slot.items
+      }
+      axios.put("http://rpg.dut-info.cf/rpg/persos/updateslot", data)
+      .then(response => {
+        if (response.data.err == 1) {
+          alert("cannot update the slot shop");
+        }
+        else {
+          alert("slot updated");
+        }
+      })
     }
   }
 });
